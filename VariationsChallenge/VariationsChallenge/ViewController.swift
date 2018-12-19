96d2fad03fe8898be2fb2bc3c3068355bb4181e6
Edudls <danieljmonaghan@gmail.com>
2018-12-19 17:06:04 -0500

    Add files via upload

---
+++ VariationsChallenge/VariationsChallenge/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  VariationsChallenge
+//
+//  Created by C02GC0VZDRJL on 12/19/18.
+//  Copyright © 2018 Daniel Monaghan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    
+    
+    
+    
+    
+}
+
